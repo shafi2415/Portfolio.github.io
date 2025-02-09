c78b120e5d92acb7bf3a6997e4d6f4e552afc995
shafi2415 <153699325+shafi2415@users.noreply.github.com>
2025-02-10 02:30:39 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,6 +1,13 @@
 const navLinks = document.querySelectorAll('header nav a');
 const logoLink = document.querySelector('.logo');
 const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
 
 const activePage = () => {
     const header = document.querySelector('header');
@@ -29,6 +36,9 @@ const activePage = () => {
     setTimeout(() => {
         barsBox.classList.add('active');
     }, 1100);
+    
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');  
 }
 
 navLinks.forEach((link, idx) => {
