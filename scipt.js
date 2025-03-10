17df0bdb586b7c5bd592a0f49aac056304a00987
shafi2415 <153699325+shafi2415@users.noreply.github.com>
2025-03-10 10:12:52 +0530

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -130,3 +130,32 @@ arrowLeft.addEventListener('click', () => {
     }
     activePortfolio();
 });
+
+const form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+    
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "shafisayd2415@gmail.com",
+        Password : "Shafi@2415",
+        To : 'mshafisyd@gmail.com',
+        From : "shafisayd2415@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    sendEmail();
+});
